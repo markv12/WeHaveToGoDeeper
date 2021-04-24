29a3eb164b4e73c6b7aad2fedd2c3af9187e2856
Scott Steffes <markv12@users.noreply.github.com>
2021-04-24 18:56:51 +0900

    add start button

--- Assets/Scripts/MainMenuUI.cs
+++ Assets/Scripts/MainMenuUI.cs
@@ -1,6 +1,18 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuUI : MonoBehaviour {
+
+    public Button startButton;
+
+    private void Awake() {
+        startButton.onClick.AddListener(delegate { StartGame(); });
+    }
+
+    private void StartGame() {
+        SceneLoader.Instance.LoadScene("GameScene");
+    }
+
     void Start() {
         DeathPointsLoader.Instance.EnsureDeathPoints();
         SessionData.playerName = "A Player";
