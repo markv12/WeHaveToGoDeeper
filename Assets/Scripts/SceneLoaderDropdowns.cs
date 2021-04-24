485b183abd1460d8b8e9ba4cb01358d9934645b3
Scott Steffes <markv12@users.noreply.github.com>
2021-04-24 16:36:42 +0900

    Rename

--- Assets/Scripts/SceneLoaderDropdowns.cs
+++ Assets/Scripts/SceneLoaderDropdowns.cs
@@ -4,8 +4,10 @@ namespace KingdomOfNight
     public partial class SceneLoader
     {
 #if UNITY_EDITOR
-        [MenuItem("Scenes/MainScene")]
-        public static void LoadMainScene() { OpenScene("Assets/scenes/MainScene.unity"); }
+        [MenuItem("Scenes/GameScene")]
+        public static void LoadGameScene() { OpenScene("Assets/scenes/GameScene.unity"); }
+        [MenuItem("Scenes/MainMenu")]
+        public static void LoadMainMenu() { OpenScene("Assets/scenes/MainMenu.unity"); }
 #endif
     }
 }
