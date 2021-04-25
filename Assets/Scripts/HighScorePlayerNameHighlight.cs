bf6cf2dadf90078cc0ca29ae726f578bdf9c3c6f
Jasper Stephenson <jasperstephenson@gmail.com>
2021-04-25 17:44:47 +0900

    ®´†®¥†¨ƒ¥ˆ©˙∆√∆˙∫

--- Assets/Scripts/HighScorePlayerNameHighlight.cs
+++ Assets/Scripts/HighScorePlayerNameHighlight.cs
@@ -2,9 +2,10 @@ using UnityEngine;
 
 public class HighScorePlayerNameHighlight : MonoBehaviour {
     public TMPro.TextMeshProUGUI text;
+    public GameObject panel;
 
     void Start() {
         if (text.text == SessionData.playerName)
-            text.color = new Color32(246, 148, 94, 255);
+            panel.SetActive(true);
     }
 }
