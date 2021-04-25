87912d9a650afbc13502c234f66bf2b2a817a49a
Jasper Stephenson <jasperstephenson@gmail.com>
2021-04-25 11:47:23 +0900

    Time on finish screen

--- Assets/Scripts/FinishManager.cs
+++ Assets/Scripts/FinishManager.cs
@@ -6,10 +6,15 @@ using UnityEngine.UI;
 public class FinishManager : MonoBehaviour {
     public Button restartButton;
     public HighScoreList highScoreList;
+    public TMPro.TextMeshProUGUI timeLabel;
 
     void Awake() {
         restartButton.onClick.AddListener(RestartGame);
         float time = Time.time - SessionData.startTime;
+
+        timeLabel.text = time.ToString("N2") + "s";
+
+        if (time < 0.1f) return;
 
         StartCoroutine(
             ScoresLoader.Instance.AddHighScoreToServer(
