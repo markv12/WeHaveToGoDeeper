1f964f5ef8276b20eeda529a51817bd1796eddf2
Jasper Stephenson <jasperstephenson@gmail.com>
2021-04-26 10:51:26 +0900

    Minor audio & text updates

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -118,7 +118,7 @@ public class UIManager : MonoBehaviour {
     private float GetWaitTimeForText(string theText) {
         int charCount = theText.Length;
         float amount = Mathf.InverseLerp(0f, 80f, charCount);
-        return 1.2f + Mathf.Lerp(0f, 3f, amount);
+        return 1.0f + Mathf.Lerp(0f, 2.5f, amount);
     }
 
     public bool IsDialoguePlaying {
